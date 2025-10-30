1cffd31d994358b720afc6fc0241ec8237d8c0b6
Nathan Hammond <nathan.hammond@czbiohub.org>
2025-10-30 14:41:30 -0700

    remove broken gres.go module

--- main.go
+++ main.go
@@ -34,7 +34,6 @@ func init() {
         prometheus.MustRegister(NewSchedulerCollector())  // from scheduler.go
 //        prometheus.MustRegister(NewFairShareCollector())  // from sshare.go
         prometheus.MustRegister(NewUsersCollector())      // from users.go
-        prometheus.MustRegister(NewGresCollector())       // from gres.go
 //        prometheus.MustRegister(NewUserJobsCollector())   // from user_jobs.go - NEW!
 }
 
