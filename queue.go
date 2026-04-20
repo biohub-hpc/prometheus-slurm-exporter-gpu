898003e47184e5a4b13d44d8bde65e18736c4d8d
Nathan Hammond <nathan.hammond@czbiohub.org>
2026-04-20 14:01:32 -0700

    remove terminal states from slurm_exporter queue.go

--- queue.go
+++ queue.go
@@ -16,11 +16,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 package main
 
 import (
-	"github.com/prometheus/client_golang/prometheus"
 	"io/ioutil"
 	"log"
 	"os/exec"
 	"strings"
+
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 type QueueMetrics struct {
@@ -28,14 +29,8 @@ type QueueMetrics struct {
 	pending_dep float64
 	running     float64
 	suspended   float64
-	cancelled   float64
 	completing  float64
-	completed   float64
 	configuring float64
-	failed      float64
-	timeout     float64
-	preempted   float64
-	node_fail   float64
 }
 
 // Returns the scheduler metrics
@@ -60,22 +55,10 @@ func ParseQueueMetrics(input []byte) *QueueMetrics {
 				qm.running++
 			case "SUSPENDED":
 				qm.suspended++
-			case "CANCELLED":
-				qm.cancelled++
 			case "COMPLETING":
 				qm.completing++
-			case "COMPLETED":
-				qm.completed++
 			case "CONFIGURING":
 				qm.configuring++
-			case "FAILED":
-				qm.failed++
-			case "TIMEOUT":
-				qm.timeout++
-			case "PREEMPTED":
-				qm.preempted++
-			case "NODE_FAIL":
-				qm.node_fail++
 			}
 		}
 	}
@@ -111,14 +94,8 @@ func NewQueueCollector() *QueueCollector {
 		pending_dep: prometheus.NewDesc("slurm_queue_pending_dependency", "Pending jobs because of dependency in queue", nil, nil),
 		running:     prometheus.NewDesc("slurm_queue_running", "Running jobs in the cluster", nil, nil),
 		suspended:   prometheus.NewDesc("slurm_queue_suspended", "Suspended jobs in the cluster", nil, nil),
-		cancelled:   prometheus.NewDesc("slurm_queue_cancelled", "Cancelled jobs in the cluster", nil, nil),
 		completing:  prometheus.NewDesc("slurm_queue_completing", "Completing jobs in the cluster", nil, nil),
-		completed:   prometheus.NewDesc("slurm_queue_completed", "Completed jobs in the cluster", nil, nil),
 		configuring: prometheus.NewDesc("slurm_queue_configuring", "Configuring jobs in the cluster", nil, nil),
-		failed:      prometheus.NewDesc("slurm_queue_failed", "Number of failed jobs", nil, nil),
-		timeout:     prometheus.NewDesc("slurm_queue_timeout", "Jobs stopped by timeout", nil, nil),
-		preempted:   prometheus.NewDesc("slurm_queue_preempted", "Number of preempted jobs", nil, nil),
-		node_fail:   prometheus.NewDesc("slurm_queue_node_fail", "Number of jobs stopped due to node fail", nil, nil),
 	}
 }
 
@@ -127,14 +104,8 @@ type QueueCollector struct {
 	pending_dep *prometheus.Desc
 	running     *prometheus.Desc
 	suspended   *prometheus.Desc
-	cancelled   *prometheus.Desc
 	completing  *prometheus.Desc
-	completed   *prometheus.Desc
 	configuring *prometheus.Desc
-	failed      *prometheus.Desc
-	timeout     *prometheus.Desc
-	preempted   *prometheus.Desc
-	node_fail   *prometheus.Desc
 }
 
 func (qc *QueueCollector) Describe(ch chan<- *prometheus.Desc) {
@@ -142,14 +113,8 @@ func (qc *QueueCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- qc.pending_dep
 	ch <- qc.running
 	ch <- qc.suspended
-	ch <- qc.cancelled
 	ch <- qc.completing
-	ch <- qc.completed
 	ch <- qc.configuring
-	ch <- qc.failed
-	ch <- qc.timeout
-	ch <- qc.preempted
-	ch <- qc.node_fail
 }
 
 func (qc *QueueCollector) Collect(ch chan<- prometheus.Metric) {
@@ -158,12 +123,6 @@ func (qc *QueueCollector) Collect(ch chan<- prometheus.Metric) {
 	ch <- prometheus.MustNewConstMetric(qc.pending_dep, prometheus.GaugeValue, qm.pending_dep)
 	ch <- prometheus.MustNewConstMetric(qc.running, prometheus.GaugeValue, qm.running)
 	ch <- prometheus.MustNewConstMetric(qc.suspended, prometheus.GaugeValue, qm.suspended)
-	ch <- prometheus.MustNewConstMetric(qc.cancelled, prometheus.GaugeValue, qm.cancelled)
 	ch <- prometheus.MustNewConstMetric(qc.completing, prometheus.GaugeValue, qm.completing)
-	ch <- prometheus.MustNewConstMetric(qc.completed, prometheus.GaugeValue, qm.completed)
 	ch <- prometheus.MustNewConstMetric(qc.configuring, prometheus.GaugeValue, qm.configuring)
-	ch <- prometheus.MustNewConstMetric(qc.failed, prometheus.GaugeValue, qm.failed)
-	ch <- prometheus.MustNewConstMetric(qc.timeout, prometheus.GaugeValue, qm.timeout)
-	ch <- prometheus.MustNewConstMetric(qc.preempted, prometheus.GaugeValue, qm.preempted)
-	ch <- prometheus.MustNewConstMetric(qc.node_fail, prometheus.GaugeValue, qm.node_fail)
 }
